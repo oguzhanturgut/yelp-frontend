70d3a96a3e6e5b238ae0fd7b13a7b0ad5988cbcf
Oguzhan <oguzhanturgut@outlook.com>
2020-02-17 00:11:34 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import LandingPage from './LandingPage/LandingPage';
+import Search from './Search/Search';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Switch>
+      <Route path='/' exact component={LandingPage} />
+      <Route path='/search' component={Search} />
+    </Switch>
+  );
 }
 
 export default App;
