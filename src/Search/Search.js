4f6fd89a97af5307f9872eaf1168fbec7c4e5e7d
Oguzhan <oguzhanturgut@outlook.com>
2020-02-17 16:24:25 +0100

    add sub nav

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
+import SubNav from '../NavBar/SubNav/SubNav';
 
 const Search = () => {
   return (
     <div>
       <NavBar />
+      <SubNav />
     </div>
   );
 };
