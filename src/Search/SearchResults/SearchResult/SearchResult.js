8c78fd170bee06a0fb7720f8f9affb2420556a1a
Oguzhan <oguzhanturgut@outlook.com>
2020-03-23 13:30:40 +0100

    yelp api connection

--- src/Search/SearchResults/SearchResult/SearchResult.js
+++ src/Search/SearchResults/SearchResult/SearchResult.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './SearchResult.module.css';
-import BusinessRating from './BusinessRating/BusinessRating';
+import BusinessRating from '../../../BusinessRating/BusinessRating';
 
 const SearchResult = () => {
   return (
