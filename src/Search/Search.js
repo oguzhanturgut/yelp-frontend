26cfd832c45d262cdde4f7f3e5cd45396554d044
Oguzhan <oguzhanturgut@outlook.com>
2020-03-24 14:12:26 +0100

    wire search results with hook

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -5,18 +5,23 @@ import SubNav from '../NavBar/SubNav/SubNav';
 import SearchResultsSummary from './SearchResultsSummary/SearchResultsSummary';
 import SearchResults from './SearchResults/SearchResults';
 
+import {useBusinessSearch} from "../hooks/yelp-api/useBusinessSearch";
+
+
 const Search = () => {
     const {location} = useReactRouter();
     const params = new URLSearchParams(location.search);
     const term = params.get('find_desc');
     const locationParam = params.get('find_loc');
 
+  const [businesses, totalResults, searchParams, setSearchParams] = useBusinessSearch(term, locationParam);
+
   return (
     <div>
       <NavBar term={term} location={locationParam}/>
-      <SubNav />
+      <SubNav/>
       <SearchResultsSummary term={term} location={locationParam}/>
-      <SearchResults />
+      <SearchResults businesses={businesses}/>
     </div>
   );
 };
--- src/Search/SearchResults/SearchResults.js
+++ src/Search/SearchResults/SearchResults.js
@@ -2,14 +2,21 @@ import React from 'react';
 import SearchResult from './SearchResult/SearchResult';
 import styles from './SearchResults.module.css';
 
-const SearchResults = () => {
+const SearchResults = props => {
+  if (!props.businesses || props.businesses.length === 0) {
+    return <div>
+      Nothing to show...
+    </div>;
+  }
+
+  const searchResults = props.businesses.map(el => <SearchResult key={el.id}
+                                                                 business={el}/>);
+
   return (
     <div className={styles['search-results']}>
-      <SearchResult />
-      <SearchResult />
-      <SearchResult />
+      {searchResults}
     </div>
   );
-};
+  };
 
 export default SearchResults;
